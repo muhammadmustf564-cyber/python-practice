d873842a7804adc7c39e4487ac754b84daa4b609
muhammadmustf564-cyber <muhammadmustf564@gmail.com>
2026-02-02 11:14:34 +0500

    Updated password strength logic with user input

--- Password_check.py
+++ Password_check.py
@@ -1,6 +1,7 @@
 import string
-a = input("Enter Password:")
-if len(a) <= 8:
-    print("Strong Password")  
+x = input("Enter password:")
+special_chars = "#$@!%^/?"
+if  len(x) <= 9 and any(char in special_chars for char in x) and any(char.isupper() for char in x)and any(char.islower() for char in x) and any(char.isdigit() for char in x):
+     print(x,"=> strong password")
 else:
-    print("Weak Password")
+    print(x,"=> weak password")
